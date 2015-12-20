18b68afb51d73c7f5f8d0ff6f502d34aeb58cf83
vojtiik <vojtechmares@googlemail.com>
2015-12-20 17:25:09 +0000

    auto scroll

--- js/lazyload.js
+++ js/lazyload.js
@@ -1,22 +1,35 @@
-
 var images = document.getElementsByClassName('galleryImage');
 var gallery = document.getElementById('scroll');
-var isScrolled = false;
+var isScrolled = true;
+var defaultIntervals = [1000, 5000, 10000, 50000]
 
 gallery.onscroll = function() {
-     isScrolled = true;
+  isScrolled = true;
 };
 
 function onWindowScroll() {
-    if (isScrolled) {
-        isScrolled = false;
-        var scrollLeft = gallery.pageXOffset ? gallery.pageXOffset : gallery.scrollLeft ? gallery.scrollLeft : 0;
-        for (var i = 0; i < images.length; i++)
-            if (scrollLeft + document.documentElement.clientWidth - images[i].offsetLeft > 0)
-                images[i].className = images[i].className.replace('lazyImg','');
+  if (isScrolled) {
+    isScrolled = false;
+    var scrollLeft = gallery.pageXOffset ? gallery.pageXOffset : gallery.scrollLeft ? gallery.scrollLeft : 0;
+    for (var i = 0; i < images.length; i++) {
+      if (scrollLeft + document.documentElement.clientWidth - images[i].offsetLeft > 0) {
+        images[i].className = images[i].className.replace('lazyImg', '');
+      }
     }
+  }
 }
 
-isScrolled = true;
-setInterval(onWindowScroll, 100);
+function onTimerScroll() {
+  if ((gallery.pageXOffset ? gallery.scrollLeft : gallery.scrollLeft) % 150 == 0) {
+    gallery.scrollLeft += 150;
+    var scrollLeft = gallery.pageXOffset ? gallery.pageXOffset : gallery.scrollLeft ? gallery.scrollLeft : 0;
+    for (var i = 0; i < images.length; i++) {
+      if (scrollLeft + document.documentElement.clientWidth - images[i].offsetLeft > 0) {
+        images[i].className = images[i].className.replace('lazyImg', '');
+      }
+    }
+  }
+}
 
+setInterval(onWindowScroll, 100);
+setInterval(onTimerScroll, 3000);
